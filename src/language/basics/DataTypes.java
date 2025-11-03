b661f7e7a47c93a5295d4c4f3b25f8e7c87a75a6
Snehapooja Patil <Snehapooja.Patil@billennium.com>
2025-11-03 16:22:01 +0530

    DataTypes

--- src/language/basics/DataTypes.java
+++ src/language/basics/DataTypes.java
@@ -19,96 +19,87 @@ public class DataTypes {
 		
 		//Temperature of a city in degree celsius : 25.5
 		float tempCity = 25.5f;
-		System.out.println(tempCity);
+		System.out.println("Temperature of a city in degree celsius:"+ tempCity);
 		
 		//Whether customer has placed an order: true /false
 		boolean custOrder = true;
-		System.out.println(custOrder);
+		System.out.println("customer has placed an order:"+custOrder);
 		
 		//Person's phone number :"123-456-7890"
 		String phNumber = "123-456-7890";
-		System.out.println(phNumber);
+		System.out.println("My Phone Number:"+phNumber);
 		
 		//Amount of money in a customers bank account : 1000.50
 		float moneyBank = 1000.50f;
-		System.out.println(moneyBank);
+		System.out.println("Amount of money in a customers bank account:"+moneyBank);
 		
 		//Person's email address : "john.doe@example.com"
 		String custEmail = "john.doe@example.com";
-		System.out.println(custEmail);
+		System.out.println("Customer Email:"+custEmail);
 		
 		
 		//Coordinate of a location (latitude, longitude) : 37.7749, -122.4194
 		float latitude = 37.7749f;
 		float longitude = -122.4194f;
-		System.out.println(latitude);
-		System.out.println(longitude);
+		System.out.println("location:"+latitude);
+		System.out.println("location:"+longitude);
 		
 		//Person's marital status : true/false
 		boolean marriageStatus = false;
-		System.out.println(marriageStatus);
+		System.out.println("Marital Status:"+marriageStatus);
 		
 		//Person's Occupation : Software Engineer
 		String occupation = "Software Engineer";
-		System.out.println(occupation);
+		System.out.println("My occupation is :"+occupation);
 		
 		//Person's favourite color : "Blue"
         String favColor = "Blue";
-        System.out.println(favColor);
+        System.out.println("Favourite color is :"+favColor);
         
         //Current year = 2025
         short curYear = 2025;
-        System.out.println(2025);
+        System.out.println("Current Year is :"+2025);
 
         //Number of followers on social media platform : 1,000,000
         int numFollowers = 1_000_000; //value with comma is not working
-        System.out.println(numFollowers);
+        System.out.println("No of followers are:"+numFollowers);
         
         //Rating of a movie : 7.5
         float movieRate = 7.5f;
-        System.out.println(movieRate);
+        System.out.println("Movie Rate:"+movieRate);
         
         //Person's blood type = 'A'
         char bloodType = 'A';
-        System.out.println(bloodType);
+        System.out.println("My blood group:"+bloodType);
         
         //Title of a book : "To Kill a Mockingbird"
         String bookTitle = "To Kill a Mockingbird";
-        System.out.println(bookTitle);
+        System.out.println("My book title:"+bookTitle);
         
         //Number of employees in a company : 500
         int numEmp = 500;
-        System.out.println(numEmp);
+        System.out.println("Number of employees :"+numEmp);
         
         //Time of an event : 2:30 PM
         String timeEvent = "2:30 PM";
-        System.out.println(timeEvent);
+        System.out.println("Time of an event :"+timeEvent);
         
         //Name of a country: "United States"
         String countryName = "United States";
-        System.out.println(countryName);
+        System.out.println("Country Name:"+countryName);
         
         //Person's eye color : "Brown"
         String eyeColor = "Brown";
-        System.out.println(eyeColor);
+        System.out.println("Eye color is :"+eyeColor);
         
         //Person's Birthplace : "New York City"
         String birthPlace = "New York City";
-        System.out.println(birthPlace);
+        System.out.println("Birthplace is:"+birthPlace);
         
         //Distance between two cities : 200.5
         float dist= 200.5f;
         System.out.println("Distance between two cities:" + dist);
-        		
- 
-        
-        
-        
-        
-        
-        
-        
-        
+           
         
         
 		
